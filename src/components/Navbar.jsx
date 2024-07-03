7aee9c0cdd129fcede221f04434f1ed0cc272e16
Mr-RoyShuvro <shuvroroyshuvo@gmail.com>
2024-07-03 13:31:23 +0600

    added navbar with logo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import { NavLink } from "react-router-dom";
+import logo from '../assets/logo3.png'
+
+
+const Navbar = () => {
+
+    const navLinks = <>
+        <NavLink to='/'><li><button>Home</button></li></NavLink>
+        <NavLink to='/add'><li><button>Add Product</button></li></NavLink>
+        <NavLink to='/mycart'><li><button>My Cart</button></li></NavLink>
+    </>
+
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        {navLinks}
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-xl"><img className="w-44 h-12" src={logo} alt="" /></a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {navLinks}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Login</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
